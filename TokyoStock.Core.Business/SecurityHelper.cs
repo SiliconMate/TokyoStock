7c4ef4c3b755bfa32a7b39f761d692c6a13eeaed
FranM4rt <mjuanfrancisco45@gmail.com>
2024-06-21 20:56:44 -0300

    Agregando clase para Hash y Salt

---
+++ TokyoStock.Core.Business/SecurityHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TokyoStock.Core.Business
+{
+    public class SecurityHelper
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[16];
+                rng.GetBytes(salt);
+                return salt;
+            }
+        }
+
+        public static byte[] HashPassword(string password, byte[] salt)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[passwordBytes.Length + salt.Length];
+
+            Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+            Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+            byte[] hashedBytes = SHA256.HashData(saltedPassword);
+
+            return hashedBytes;
+        }
+
+        public static string HashPassword1Hex(string password, byte[] salt)
+        {
+            return Convert.ToBase64String(HashPassword(password, salt));
+        }
+
+    }
+}
