1efb9bc76ba2962d7418a5866148872362c63f08
FranM4rt <mjuanfrancisco45@gmail.com>
2024-06-21 18:22:07 -0300

    Agregando venta y compra en repository y proyecto Test

---
+++ TokyoStock.Core.Data/VentaRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using TokyoStock.Core.Entities;
+
+namespace TokyoStock.Core.Data
+{
+    public class VentaRepository
+    {
+        public VentaRepository()
+        {
+
+        }
+
+        public List<Venta> GetVentas()
+        {
+            var ventas = new List<Venta>();
+
+            using (var db = new TokyoStockContext())
+            {
+                ventas = db.Venta.ToList();
+            }
+
+            return ventas;
+        }
+
+    }
+}
