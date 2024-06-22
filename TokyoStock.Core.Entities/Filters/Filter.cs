ed16678e74a6f8da9496acdccec0f5e93f7ca1e3
FranM4rt <mjuanfrancisco45@gmail.com>
2024-06-22 11:38:52 -0300

    Creando la clase para parametros del paginado

---
+++ TokyoStock.Core.Entities/Filters/Filter.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TokyoStock.Core.Entities.Filters
+{
+    public class Filter
+    {
+        public string TextToSearch { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
