513572eaf880cc159f9e5ce97c56c8229be5f4c1
gero123-diabe <guastavinogeronimo1@gmail.com>
2024-06-22 23:13:18 -0300

    Agregue un metodo para calcular el Stock

--- TokyoStock.Core.Business/ProductoBusiness.cs
+++ TokyoStock.Core.Business/ProductoBusiness.cs
@@ -6,10 +6,17 @@ namespace TokyoStock.Core.Business
     public class ProductoBusiness
     {
         private readonly ProductoRepository _pr;
-
+        private readonly CompraRepository _cr;
+        private readonly VentaRepository _vr;
         public ProductoBusiness(ProductoRepository pr)
         {
             _pr = pr;
+        }
+        public ProductoBusiness(ProductoRepository pr, CompraRepository cr, VentaRepository vr)
+        {
+            _pr = pr;
+            _cr = cr;
+            _vr = vr;
         }
 
         public List<Producto> GetProductos()
