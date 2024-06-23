c25490c86959921be43c1a6dff47fe34df97fecb
FranM4rt <mjuanfrancisco45@gmail.com>
2024-06-22 21:36:00 -0300

    agregando business de compra y venta

---
+++ TokyoStock.Core.Business/CompraBusiness.cs
@@ -0,0 +1,27 @@
+﻿
+using TokyoStock.Core.Data;
+using TokyoStock.Core.Entities.Filters;
+using TokyoStock.Core.Entities;
+
+namespace TokyoStock.Core.Business
+{
+    public class CompraBusiness
+    {
+        private readonly CompraRepository _cr;
+
+        public CompraBusiness(CompraRepository cr)
+        {
+            _cr = cr;
+        }
+
+        public List<Compra> GetCompras()
+        {
+            return _cr.GetCompras();
+        }
+
+        public (List<Compra> list, int total) GetComprasByFilter(Filter f)
+        {
+            return GetComprasByFilter(f);
+        }
+    }
+}
---
+++ TokyoStock.Core.Business/VentaBusiness.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TokyoStock.Core.Data;
+using TokyoStock.Core.Entities;
+using TokyoStock.Core.Entities.Filters;
+
+namespace TokyoStock.Core.Business
+{
+    public class VentaBusiness
+    {
+        private readonly VentaRepository _vr;
+
+        public VentaBusiness(VentaRepository vr)
+        {
+            _vr = vr;
+        }
+
+        public List<Venta> GetVentas()
+        {
+            return _vr.GetVentas();
+        }
+
+        public (List<Venta> list, int total) GetVentasByFilter(Filter f)
+        {
+            return GetVentasByFilter(f);
+        }
+    }
+}
